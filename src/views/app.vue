87c87901249ad3b89cb59d9fc393675d52d2b6ef
wilian <wilian.silva@cedrotech.com>
2020-01-16 00:43:57 -0200

    Adding tree component

---
+++ src/views/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <section id="app">
+    <router-view/>
+  </section>
+</template>
+
+<style lang="scss">
+html {
+  &, body, body section#app { margin: 0; padding: 0; display: block; width: 100%; height: 100%; max-height: 100%; }
+}
+</style>
